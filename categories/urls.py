b8099c2c41e67a5e2b9681508f1d9fe35ac88f19
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-03-13 22:43:05 +0600

    CRUD ops done on category Model

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . views import category_form_creating, showing_all_category
- 
+from . views import update_category, delete_category
 urlpatterns = [
-    path('create_category/',category_form_creating, name="category_create"),
-    path('showing_categories/', showing_all_category, name="category_show"),
+    path('create/',category_form_creating, name="category_create"),
+    path('show/', showing_all_category, name="category_show"),
+    path('update/<int:id>/',update_category, name="category_update"),
+    path('delete/<int:id>/', delete_category, name="category_delete"),
+    
+    
+    
 ]
--- categories/views.py
+++ categories/views.py
@@ -14,8 +14,30 @@ def category_form_creating(request):
         form = CategoryForm()
     return render(request, 'categories/category.html', {'form':form})
 
+
 def showing_all_category(request):
     all_category = Category.objects.all()
     return render(request,'categories/category.html', {'all_cat':all_category})
+
+
+def update_category(request, id):
+    category = Category.objects.get(pk=id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_show')
+    else:
+        form = CategoryForm(instance= category)
+    return render(request,'categories/category.html', {'form':form})
+
+
+
+def delete_category(request, id):
+    category = Category.objects.get(pk = id)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_show')
+    return render(request,'categories/category_delete.html', {'category':category})
 
 
